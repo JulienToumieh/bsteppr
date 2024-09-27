b7cf6ccee75f464fab9acb6f482491a7a4939d37
JulienToumieh <julientoumieh2@gmail.com>
2024-09-27 22:29:51 +0300

    Tested a timer node based solution

--- Globals.gd
+++ Globals.gd
@@ -114,6 +114,7 @@ var beatF = [
 	["0000","0000","0000","0000","0000","0000","0000","0000"]
 ]
 
+'''
 var activeBeat = beatA
 
 var playbackPosition = 0
@@ -148,6 +149,60 @@ func _process(delta):
 			last_tick_time = current_time
 			tick()
 
+
+func playSound(id):
+	match id:
+		1: get_node("Instrument1").play()
+		2: get_node("Instrument2").play()
+		3: get_node("Instrument3").play()
+		4: get_node("Instrument4").play()
+		5: get_node("Instrument5").play()
+		6: get_node("Instrument6").play()
+		7: get_node("Instrument7").play()
+		8: get_node("Instrument8").play()
+'''
+
+
+
+'
+var beats = {
+	"A": [["0000"] * 8 for _ in range(16)],
+	"B": [["0000"] * 8 for _ in range(16)],
+	"C": [["0000"] * 8 for _ in range(16)],
+	"D": [["0000"] * 8 for _ in range(16)],
+	"E": [["0000"] * 8 for _ in range(16)],
+	"F": [["0000"] * 8 for _ in range(16)]
+}
+'
+
+var activeBeat = beatA
+var playbackPosition = 0
+var playing = false
+
+var tempo = 60000 / 128  # Tempo in milliseconds
+
+
+@onready var metronome_timer = $Timer  # Reference to the Timer node
+
+func togglePlayback():
+	playing = !playing
+	if playing:
+		metronome_timer.start()  # Start the timer
+	else:
+		playbackPosition = 0
+		metronome_timer.stop()  # Stop the timer
+
+func tick():
+	playbackPosition = (playbackPosition % 16) + 1
+	if playbackPosition % 4 == 0:
+		playSound(3)
+
+func _ready():
+	metronome_timer.wait_time = tempo / 4000.0  # Set wait time in seconds
+	metronome_timer.timeout.connect(self._on_Timer_timeout)
+
+func _on_Timer_timeout():
+	tick()  # Call tick on timer timeout
 
 func playSound(id):
 	match id:
