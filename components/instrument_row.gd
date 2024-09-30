79a5da55e0fcfc196f3436ba4ac407ee998c3b9d
JulienToumieh <julientoumieh2@gmail.com>
2024-09-30 08:38:14 +0300

    Fixed instrument colors

--- components/instrument_row.gd
+++ components/instrument_row.gd
@@ -3,14 +3,14 @@ extends Node2D
 @export var instrumentID = 0
 
 func _ready():
-	if instrumentID == 0: modulate = Color("FF4141")
-	if instrumentID == 1: modulate = Color("FF44BF")
-	if instrumentID == 2: modulate = Color("854BFF")
-	if instrumentID == 3: modulate = Color("3385FF")
-	if instrumentID == 4: modulate = Color("00F0FF")
-	if instrumentID == 5: modulate = Color("3CFF72")
-	if instrumentID == 6: modulate = Color("4CFF2F")
-	if instrumentID == 7: modulate = Color("#FF922D")
+	if instrumentID == 1: modulate = Color("FF4141")
+	if instrumentID == 2: modulate = Color("FF44BF")
+	if instrumentID == 3: modulate = Color("854BFF")
+	if instrumentID == 4: modulate = Color("3385FF")
+	if instrumentID == 5: modulate = Color("00F0FF")
+	if instrumentID == 6: modulate = Color("3CFF72")
+	if instrumentID == 7: modulate = Color("4CFF2F")
+	if instrumentID == 8: modulate = Color("#FF922D")
 
 
 func _process(delta):
