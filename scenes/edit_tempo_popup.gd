4540a467643418f545f970b92551da56b30ea170
JulienToumieh <julientoumieh2@gmail.com>
2024-10-09 16:22:59 +0300

    Added tap tempo button

--- scenes/edit_tempo_popup.gd
+++ scenes/edit_tempo_popup.gd
@@ -6,6 +6,8 @@ var swing = 0
 var minTempo = 20
 var maxTempo = 250
 
+var tapTempoVals = []
+
 func _ready():
 	position = get_viewport().get_size() / 2
 	get_node("TempoSlider").min_value = minTempo
@@ -13,7 +15,39 @@ func _ready():
 	bpm = Globals.bpm
 	swing = Globals.swing
 	updateGlobal()
+	tapTempoIndicator()
 
+func tapTempo():
+	tapTempoVals.append(Time.get_ticks_msec())
+	var subs = []
+	if tapTempoVals.size() >= 2:
+		for i in range(tapTempoVals.size()):
+			if i+1 != tapTempoVals.size():
+				subs.append(tapTempoVals[i+1] - tapTempoVals[i])
+		bpm = int(60000/average(subs))
+		updateGlobal()
+		
+	tapTempoIndicator()
+	
+	
+	if tapTempoVals.size() == 8:
+		subs.clear()
+		tapTempoVals.clear()
+
+
+func average(numbers: Array) -> float:
+	if numbers.size() == 0: 
+		return 0.0
+	
+	var total_sum = numbers.reduce(func(accum, number): return accum + number, 0)
+	return total_sum / numbers.size()
+
+func tapTempoIndicator():
+	for i in range(8):
+		if i < tapTempoVals.size(): 
+			get_node("TapTempoIndicator/P" + str(i+1)).modulate.a = 1
+		else:
+			get_node("TapTempoIndicator/P" + str(i+1)).modulate.a = 0.3
 
 func _on_close_popup_pressed():
 	Globals.bpm = bpm
