032577a6cb14a8363995f4b4510a1593dcbbe560
JulienToumieh <julientoumieh2@gmail.com>
2024-10-12 16:53:08 +0300

    Knob modifications and improvements

--- components/knob.gd
+++ components/knob.gd
@@ -16,18 +16,28 @@ var startPos = 0
 func _ready():
 	$KnobArrow.rotation_degrees = (pos * 2.0 / 100.0) * 150.0 - 150.0
 	var val = minVal + (maxVal - minVal) * pos / 100.0
-	$KnobVal.text = str("%.2f" % val)
+	if abs(abs(maxVal) - abs(minVal)) > 5:
+		$KnobVal.text = str(int(val))
+	else:
+		$KnobVal.text = str("%.2f" % val)
+	$Label.text = name
 
 
 func _process(delta):
 	if pressed:
-		pos += (get_global_mouse_position() - startPos).x / 2
+		if abs((get_global_mouse_position() - startPos).y) > abs((get_global_mouse_position() - startPos).x):
+			pos -= (get_global_mouse_position() - startPos).y / 2
+		else:
+			pos += (get_global_mouse_position() - startPos).x / 2
 		startPos = get_global_mouse_position()
 		if pos > 100: pos = 100
 		if pos < 0: pos = 0
 		$KnobArrow.rotation_degrees = (pos * 2.0 / 100.0) * 150.0 - 150.0
 		var val = minVal + (maxVal - minVal) * pos / 100.0
-		$KnobVal.text = str("%.2f" % val)
+		if abs(abs(maxVal) - abs(minVal)) > 5:
+			$KnobVal.text = str(int(val))
+		else:
+			$KnobVal.text = str("%.2f" % val)
 
 
 func _on_b_knob_button_down():
