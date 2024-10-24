20f7462f85dfb4e7992182ef4fe1fa731a5418cb
JulienToumieh <julientoumieh2@gmail.com>
2024-10-24 17:01:35 +0300

    Added loop repeat count slider + also immediately updates globally

--- scenes/edit_loop_popup.gd
+++ scenes/edit_loop_popup.gd
@@ -4,10 +4,13 @@ extends Node2D
 @export var loopCount = 0
 @export var nextLoop = ""
 
+var maxLoopCount = 64
+
 var loops = ["A", "B", "C", "D", "E", "F", "Ø"]
 
 func _ready():
 	position = get_viewport().get_size() / 2
+	$LoopCount/loopCountSlider.max_value = maxLoopCount
 	nextLoop = Globals.loop[loopName][1]
 	
 	for l in loops:
@@ -15,25 +18,30 @@ func _ready():
 	get_node("NextLoops/Select" + nextLoop).modulate.v = 1
 	loopCount = Globals.loop[loopName][0]
 	get_node("LoopCount/Label").text = str(loopCount)
+	
+	updateLocal()
 
 
 func updateGlobal():
 	get_node("LoopCount/Label").text = str(loopCount)
+	$LoopCount/loopCountSlider.value = loopCount
 	for l in loops:
 		get_node("NextLoops/Select" + l).modulate.v = 0.3
 	get_node("NextLoops/Select" + nextLoop).modulate.v = 1
 	Globals.loop[loopName] = [loopCount, nextLoop]
 	Globals.updateLoopChips()
+	Globals.loopCounter = Globals.loop[Globals.activeLoop][0]
 
 func updateLocal():
 	get_node("LoopCount/Label").text = str(loopCount)
 	for l in loops:
 		get_node("NextLoops/Select" + l).modulate.v = 0.3
 	get_node("NextLoops/Select" + nextLoop).modulate.v = 1
+	updateGlobal()
 
 func _on_close_popup_pressed():
+	#updateGlobal()
 	queue_free()
-	updateGlobal()
 
 func _on_sel_a_pressed():
 	nextLoop = "A"
@@ -58,11 +66,11 @@ func _on_sel_o_pressed():
 	updateLocal()
 
 func _on_increase_1_pressed():
-	if loopCount < 64: loopCount += 1
+	if loopCount < maxLoopCount: loopCount += 1
 	updateLocal()
 func _on_increase_2_pressed():
-	if loopCount < 64: loopCount += 5
-	if loopCount > 64: loopCount = 64
+	if loopCount < maxLoopCount: loopCount += 5
+	if loopCount > maxLoopCount: loopCount = maxLoopCount
 	updateLocal()
 func _on_decrease_1_pressed():
 	if loopCount != 1: loopCount -= 1
@@ -70,4 +78,9 @@ func _on_decrease_1_pressed():
 func _on_decrease_2_pressed():
 	if loopCount > 1: loopCount -= 5
 	if loopCount < 1: loopCount = 1
+	updateLocal()
+
+
+func _on_loop_count_slider_value_changed(value):
+	loopCount = $LoopCount/loopCountSlider.value
 	updateLocal()
